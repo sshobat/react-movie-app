6638c1b598bf5bd0777b9359e601efafcece427c
Srđan Šobat <sshobat@gmail.com>
2021-05-12 06:34:26 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import {Switch, Router} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      React movie app
     </div>
+    // <>
+    //   <Switch>
+    //     <Route path="/"></Route>
+    //     <Route path="/"></Route>
+    //   </Switch>
+    // </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase';
+import { BrowserRouter } from 'react-router-dom';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAH7ayWsC1dbiOeLLkgullTGoIYnExkS0A",
+  authDomain: "movie-app-7527e.firebaseapp.com",
+  projectId: "movie-app-7527e",
+  storageBucket: "movie-app-7527e.appspot.com",
+  messagingSenderId: "373292683819",
+  appId: "1:373292683819:web:ed4703f3db15703898106e"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter><App /></BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
